04c1712d982720e82f95b03f2b67184959fc939c
Marek Sebera <marek.sebera@gmail.com>
2022-01-11 22:26:31 +0100

    continuous sync

--- roh/dmx/client/consumer.py
+++ roh/dmx/client/consumer.py
@@ -6,11 +6,8 @@ from serial import PARITY_NONE, STOPBITS_TWO
 
 
 class Consumer:
-    def __init__(self):
-        pass
-
-    def run(self, sport: str):
-        ser = serial.Serial(
+    def __init__(self, sport: str):
+        self.ser = serial.Serial(
             sport,
             baudrate=250000,
             parity=PARITY_NONE,
@@ -18,31 +15,41 @@ class Consumer:
             xonxoff=True,
         )
 
-        fd = os.open(sport,  os.O_RDWR | os.O_NOCTTY | os.O_NONBLOCK)
-        iflag, oflag, cflag, lflag, ispeed, ospeed, cc = termios.tcgetattr(fd)
+        self.fd = self.ser.fd
+        # self.fd = os.open(sport, os.O_RDWR | os.O_NOCTTY | os.O_NONBLOCK)
+
+        iflag, oflag, cflag, lflag, ispeed, ospeed, cc = termios.tcgetattr(self.fd)
         iflag |= termios.PARMRK
         iflag &= ~termios.IGNBRK
-        termios.tcsetattr(fd, termios.TCSANOW, [iflag, oflag, cflag, lflag, ispeed, ospeed, cc])
-        # termios.tcdrain(ser.fd)
+        termios.tcsetattr(self.fd, termios.TCSANOW, [iflag, oflag, cflag, lflag, ispeed, ospeed, cc])
 
-        breakbytes = b'\x00\xFF\x00'
+    def get_sync(self):
+        print('GETTING SYNC')
+        breakbytes = b'\xFF\x00\x00'
 
         while True:
-            b: bytes = os.read(fd, 3)
+            b: bytes = os.read(self.fd, 3)
             if b == breakbytes:
                 print("SYNC")
                 break
             print(b.hex(), end='', flush=True)
 
+    def run(self):
+        # self.get_sync()
+        correct_tail = b'\xFF\x00\x00'
+
         while True:
-            b: bytes = ser.read(516)
-            for i in range(0, 514):
+            b: bytes = self.ser.read(516)
+            if b[513:] != correct_tail:
+                print("tail %s" % b[513:].hex())
+                self.get_sync()
+                continue
+
+            for i in range(0, 513):
                 if b[i] != 0:
-                    print("%d val %d" % (i, b[i]))
-
-
+                    print("idx:%d val:%d" % (i, b[i]))
 
 
 if __name__ == '__main__':
-    c: Consumer = Consumer()
-    c.run('/dev/ttyUSB0')
+    c: Consumer = Consumer('/dev/ttyUSB0')
+    c.run()
