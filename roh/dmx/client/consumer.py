7b8582dfe1159b697739a4e22aea860fd4461c97
Marek Sebera <marek.sebera@gmail.com>
2022-01-11 22:02:22 +0100

    sync detect once

--- roh/dmx/client/consumer.py
+++ roh/dmx/client/consumer.py
@@ -1,31 +1,46 @@
+import os
+import termios
+
 import serial
 from serial import PARITY_NONE, STOPBITS_TWO
 
 
 class Consumer:
-    """
-
-    """
+    def __init__(self):
+        pass
 
     def run(self, sport: str):
-        p = serial.Serial(
-            port=sport,
+        ser = serial.Serial(
+            sport,
             baudrate=250000,
             parity=PARITY_NONE,
             stopbits=STOPBITS_TWO,
+            xonxoff=True,
         )
-        nb = bytes([0])
-        c = 0
+
+        fd = os.open(sport,  os.O_RDWR | os.O_NOCTTY | os.O_NONBLOCK)
+        iflag, oflag, cflag, lflag, ispeed, ospeed, cc = termios.tcgetattr(fd)
+        iflag |= termios.PARMRK
+        iflag &= ~termios.IGNBRK
+        termios.tcsetattr(fd, termios.TCSANOW, [iflag, oflag, cflag, lflag, ispeed, ospeed, cc])
+        # termios.tcdrain(ser.fd)
+
+        breakbytes = b'\x00\xFF\x00'
+
         while True:
-            r = p.read()
-            if r == nb:
-                c += 1
-                continue
-            if r == bytes([0xA7]):
-                print("stop at %d" % (c-7))
-                c = 7
-            print("%d:%s" % (c, r.hex()), flush=True)
-            c += 1
+            b: bytes = os.read(fd, 3)
+            if b == breakbytes:
+                print("SYNC")
+                break
+            print(b.hex(), end='', flush=True)
+
+        while True:
+            b: bytes = ser.read(516)
+            for i in range(0, 514):
+                if b[i] != 0:
+                    print("%d val %d" % (i, b[i]))
+
+
 
 
 if __name__ == '__main__':
